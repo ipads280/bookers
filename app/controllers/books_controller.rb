1681ed8a83ad221718fcbe659abea7bf9704006a
EC2 Default User <ec2-user@ip-172-31-83-11.ec2.internal>
2022-04-10 15:15:38 +0000

    完成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  protect_from_forgery
+  
+  def index
+    @book = Book.new
+    @books=Book.all
+
+  end
+  
+
+  def create
+    flash[:notice] = "successfully" 
+    @books=Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render:index
+    end
+
+  end
+
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    flash[:notice] = "Book was successfully updated." 
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
